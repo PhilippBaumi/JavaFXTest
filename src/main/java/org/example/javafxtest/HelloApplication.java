fdab187efc78899742071bf61815e045116052ac
PhilippBaumi <philipp.baumgartner@live.at>
2026-03-26 08:28:01 +0100

    Titel des Fensters angepasst

--- src/main/java/org/example/javafxtest/HelloApplication.java
+++ src/main/java/org/example/javafxtest/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("JavaFXTest");
         stage.setScene(scene);
         stage.show();
     }
